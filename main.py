73f48fe414db8b41f2b4c14086a5aa753318c30b
JettPy <smel_00@bk.ru>
2023-08-08 19:35:47 +0300

    all work done

--- main.py
+++ main.py
@@ -46,19 +46,31 @@ connection.commit()
 
 # Функционал бота
 
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=['start', 'help'])
 def start(message):
     bot.send_message(message.chat.id,
                      '''Привет!
-/add_course - добавить курс в базу данных
+/help - вывести список команд
+/add_course - добавить курс в базу данных (админ)
 /courses - просмотреть список доступных курсов
 /enroll - записаться на курс
-/check_clients - посмотреть базу данных клиентов
-/check_courses - посмотреть базу данных курсов''')
+/check_clients - посмотреть базу данных клиентов (админ)
+/check_courses - посмотреть базу данных курсов (админ)
+/timetable - вывести расписание курсов
+/add_lesson - добавить урок в расписание (админ)
+/check_timetable - посмотреть базу данных расписания (админ)
+/check_admins - посмотреть базу данных администраторов (админ)
+/get_superuser_rights - выдать права администратора
+/logout - выйти из режима администратора''')
 
 
 @bot.message_handler(commands=['add_course'])
 def add_course(message):
+    cursor.execute('''SELECT * FROM admins WHERE username = (?)''', (message.from_user.username,))
+    username_fetch = cursor.fetchone()
+    if username_fetch is None:
+        bot.send_message(message.chat.id, 'У вас нет прав для этого действия')
+        return
     bot.send_message(message.chat.id, 'Введите название нового курса')
     bot.register_next_step_handler(message, get_course_title)
 
@@ -76,21 +88,27 @@ def courses(message):
     answer = 'У нас есть следующие курсы:'
     cursor.execute('''SELECT title FROM courses''')
     courses = cursor.fetchall()
-    for course in courses:
-        answer += f'\n{course[0]}'
+    if len(courses) > 0:
+        for course in courses:
+            answer += f'\n{course[0]}'
+    else:
+        answer = "Курсов пока нет"
     bot.send_message(message.chat.id, answer)
 
 
 @bot.message_handler(commands=['enroll'])
 def enroll(message):
-    bot.send_message(message.chat.id, 'Для записи на курс укажите ваше имя и фамилию')
+    bot.send_message(message.chat.id, 'Для записи на курс укажите ваше имя и фамилию через пробел')
     bot.register_next_step_handler(message, get_name)
 
 
 def get_name(message):
-    name, surname = message.text.split()
-    bot.send_message(message.chat.id, 'Отлично! Теперь введите ваш телефон')
-   bot.register_next_step_handler(message, get_phone, name, surname)
+    try:
+        name, surname = message.text.split()
+        bot.send_message(message.chat.id, 'Отлично! Теперь введите ваш телефон')
+        bot.register_next_step_handler(message, get_phone, name, surname)
+    except ValueError:
+        bot.send_message(message.chat.id, 'Упс! Вы ввели что то не так попробуйте с начала')
 
 
 def get_phone(message, name, surname):
@@ -118,7 +136,12 @@ def get_course(message, name, surname, phone):
 
 
 @bot.message_handler(commands=['check_clients'])
-def check(message):
+def check_clients(message):
+    cursor.execute('''SELECT * FROM admins WHERE username = (?)''', (message.from_user.username,))
+    username_fetch = cursor.fetchone()
+    if username_fetch is None:
+        bot.send_message(message.chat.id, 'У вас нет прав для этого действия')
+        return
     answer = ''
     cursor.execute('''SELECT * FROM clients''')
     clients = cursor.fetchall()
@@ -131,7 +154,12 @@ def check(message):
 
 
 @bot.message_handler(commands=['check_courses'])
-def check(message):
+def check_courses(message):
+    cursor.execute('''SELECT * FROM admins WHERE username = (?)''', (message.from_user.username,))
+    username_fetch = cursor.fetchone()
+    if username_fetch is None:
+        bot.send_message(message.chat.id, 'У вас нет прав для этого действия')
+        return
     answer = ''
     cursor.execute('''SELECT * FROM courses''')
     courses = cursor.fetchall()
@@ -148,11 +176,12 @@ def timetable(message):
     answer = 'Наше расписание:'
     cursor.execute('''SELECT * FROM timetable''')
     timetable = cursor.fetchall()
-    for entry in timetable:
-        cursor.execute('''SELECT title FROM courses WHERE id = (?)''', (entry[0],))
-        course = cursor.fetchone()[0]
-        answer += f'\n{course} ({entry[3]} / {entry[2]}):\nАдрес: {entry[4]}'
-    if answer == 'Наше расписание:':
+    if len(timetable) > 0:
+        for entry in timetable:
+            cursor.execute('''SELECT title FROM courses WHERE id = (?)''', (entry[0],))
+            course = cursor.fetchone()[0]
+            answer += f'\n{course} ({entry[3]} / {entry[2]}):\nАдрес: {entry[4]}'
+    else:
         answer = 'Расписание отсутствует'
     bot.send_message(message.chat.id, answer)
 
@@ -175,9 +204,12 @@ def get_timetable_course(message):
 
 
 def get_timetable_date_time(message, course_title):
-    date, time = message.text.split()
-    bot.send_message(message.chat.id, 'И последнее! Введите адрес')
-    bot.register_next_step_handler(message, get_timetable_address, course_title, date, time)
+    try:
+        date, time = message.text.split()
+        bot.send_message(message.chat.id, 'И последнее! Введите адрес')
+        bot.register_next_step_handler(message, get_timetable_address, course_title, date, time)
+    except ValueError:
+        bot.send_message(message.chat.id, 'Упс! Вы ввели что то не так попробуйте с начала')
 
 
 def get_timetable_address(message, course_title, date, time):
@@ -193,6 +225,74 @@ def get_timetable_address(message, course_title, date, time):
             INSERT INTO timetable (course, time, date, address) VALUES (?, ?, ?, ?)''', (course, time, date, message.text))
     connection.commit()
     bot.send_message(message.chat.id, 'Замечательно! Вы добавили новое занятие!')
+
+
+@bot.message_handler(commands=['get_superuser_rights'])
+def get_superuser_rights(message):
+    bot.send_message(message.chat.id, 'Введите пароль администратора')
+    bot.register_next_step_handler(message, check_password)
+
+
+def check_password(message):
+    password = message.text
+    global admin_password
+    if password == admin_password:
+        cursor.execute('''SELECT * FROM admins WHERE username = (?)''', (message.from_user.username,))
+        username_fetch = cursor.fetchone()
+        if username_fetch is None:
+            cursor.execute('''INSERT INTO admins (username) VALUES (?)''', (message.from_user.username,))
+            connection.commit()
+        bot.send_message(message.chat.id, 'Вам выданы права администратора')
+    else:
+        bot.send_message(message.chat.id, 'Пароль неверный')
+    bot.delete_message(message.chat.id, message.id)
+
+
+@bot.message_handler(commands=['logout'])
+def logout(message):
+    cursor.execute('''DELETE FROM admins WHERE username = (?)''', (message.from_user.username,))
+    connection.commit()
+    bot.send_message(message.chat.id, 'Вы вышли')
+
+@bot.message_handler(commands=['check_timetable'])
+def check_timetable(message):
+    cursor.execute('''SELECT * FROM admins WHERE username = (?)''', (message.from_user.username,))
+    username_fetch = cursor.fetchone()
+    if username_fetch is None:
+        bot.send_message(message.chat.id, 'У вас нет прав для этого действия')
+        return
+    answer = ''
+    cursor.execute('''SELECT * FROM timetable''')
+    timetable = cursor.fetchall()
+    if len(timetable) > 0:
+        for entry in timetable:
+            answer += f'{entry[0]}, {entry[1]}, {entry[2]}, {entry[3]}, {entry[4]}\n'
+    else:
+        answer = 'Записей в расписании нет'
+    bot.send_message(message.chat.id, answer)
+
+
+@bot.message_handler(commands=['check_admins'])
+def check_admins(message):
+    cursor.execute('''SELECT * FROM admins WHERE username = (?)''', (message.from_user.username,))
+    username_fetch = cursor.fetchone()
+    if username_fetch is None:
+        bot.send_message(message.chat.id, 'У вас нет прав для этого действия')
+        return
+    answer = ''
+    cursor.execute('''SELECT * FROM admins''')
+    admins = cursor.fetchall()
+    if len(admins) > 0:
+        for entry in admins:
+            answer += f'{entry[0]}, {entry[1]}\n'
+    else:
+        answer = 'Записей курсов нет'
+    bot.send_message(message.chat.id, answer)
+
+
+@bot.message_handler(content_types=['text'])
+def default_message(message):
+    bot.send_message(message.chat.id, 'Я тебя не понимаю. Введи /help для помощи со списком команд')
 
 
 bot.infinity_polling()
